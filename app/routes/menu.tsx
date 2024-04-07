2b7b1ed8a7b7218fbfb49f8b2efe806a01838962
Beshoy R Mansour <beshoy.r.mansour@gmail.com>
2024-04-07 23:36:12 +0200

    create menu route

--- app/routes/menu.tsx
+++ app/routes/menu.tsx
@@ -6,8 +6,8 @@ export const meta: MetaFunction = () => [{ title: "Remix@Edge | New Remix App" }
 
 export default function Edge() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix@Edge</h1>
+    <div >
+      <h1>MENU</h1>
     </div>
   );
 }
