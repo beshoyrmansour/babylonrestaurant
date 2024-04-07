07358626d980441da15e867c922424c64e5b381c
Beshoy R Mansour <beshoy.r.mansour@gmail.com>
2024-04-07 23:49:57 +0200

    add menu data object

--- app/routes/menu.tsx
+++ app/routes/menu.tsx
@@ -1,10 +1,17 @@
-import type { MetaFunction } from "@vercel/remix";
+import { useLoaderData } from "@remix-run/react";
+import { json, type LoaderFunctionArgs, type MetaFunction } from "@vercel/remix";
+import jsonData from "~/assets/data.json";
 
 export const config = { runtime: "edge" };
 
 export const meta: MetaFunction = () => [{ title: "Remix@Edge | New Remix App" }];
+export const loader = async (args: LoaderFunctionArgs) => {
 
+  return json({ data: jsonData })
+};
 export default function Edge() {
+  const { data } = useLoaderData<typeof loader>()
+  console.log({ data, });
   return (
     <div className="flex justify-center items-center w-full h-full flex-col">
       <h1>MENU</h1>
