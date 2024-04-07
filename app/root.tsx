b085644581a3f2261f8c4b3f19e4681359671d9c
Beshoy R Mansour <beshoy.r.mansour@gmail.com>
2024-04-07 23:00:01 +0200

    init

--- app/root.tsx
+++ app/root.tsx
@@ -6,17 +6,19 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import { Analytics } from "@vercel/analytics/react";
+import './styles.css'
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" style={{ height: '100%', }}>
       <head>
+
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body style={{ height: '100%', }}>
         {children}
         <ScrollRestoration />
         <Scripts />
---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./app/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['"Reem Kufi"', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
+
